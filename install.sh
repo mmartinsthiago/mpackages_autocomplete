1b4083fe4cae84444795fe06ddc553f626dafe48
Thiago Mmendes <mmartinsthiago@gmail.com>
2024-10-31 15:57:27 -0300

    Changes mv to cp

--- install.sh
+++ install.sh
@@ -12,30 +12,30 @@ function withoutVerbose() {
 echo "Installing command mpackages ..."
 
 withoutVerbose "Changing permission to executable ..." "$1"
-withoutVerbose"sudo chmod +x mpackages" "$1"
+withoutVerbose "sudo chmod +x mpackages" "$1"
 sudo chmod +x mpackages
 withoutVerbose "Permission changed!" "$1"
 
 withoutVerbose "Moving mpackages to executable commands/bin directory ..." "$1"
-withoutVerbose "sudo mv mpackages /usr/local/bin" "$1"
-sudo mv mpackages /usr/local/bin
+withoutVerbose "sudo cp mpackages /usr/local/bin" "$1"
+sudo cp mpackages /usr/local/bin
 withoutVerbose "Moved mpackages to executable commands/bin directory!" "$1"
 
 withoutVerbose "Moving mpackages_autcomplete to bash_completion.d ..." "$1"
-withoutVerbose "sudo mv mpackages_autocomplete /etc/bash_completion.d/mpackages" "$1"
-sudo mv mpackages_autocomplete /etc/bash_completion.d/mpackages
+withoutVerbose "sudo cp mpackages_autocomplete /etc/bash_completion.d/mpackages" "$1"
+sudo cp mpackages_autocomplete /etc/bash_completion.d/mpackages
 withoutVerbose "Moved mpackages_autcomplete to bash_completion.d!" "$1"
 
-echo "Instalation successfully made!"
-echo "Commands avalilable:"
+echo "Installation successfully made!"
+echo "Commands available:"
 echo
-echo "sudo mpackages update              |   To update instalations with the pacman"
-echo "sudo mpackages install <package>   |   To install package with the pacman"
-echo "sudo mpackages remove <package>    |   To remove package with the pacman"
+echo "sudo mpackages update              |   To update installations with pacman"
+echo "sudo mpackages install <package>   |   To install a package with pacman"
+echo "sudo mpackages remove <package>    |   To remove a package with pacman"
 echo
 sleep 1
 echo "Optional"
-echo "You can delete this directory after this instalation, typing:"
+echo "You can delete this directory after this installation by typing:"
 echo "cd .. && rm -rf mpackage_autocomplete"
 
 echo "Bye!"
